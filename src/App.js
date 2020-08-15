692685b269c5e3d8d8172881c487db7ee4659dbc
caikan <caikanxp@gmail.com>
2020-08-15 12:37:04 +0800

    homework 9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 // import {useState, useEffect} from "react";
-import UseMemoPage from "./pages/UseMemoPage";
+// import UseMemoPage from "./pages/UseMemoPage";
 // import CommentListPage from "./pages/CommentListPage";
 // import PureComponentPage from "./pages/PureComponentPage";
 // import ReactMemoPage from "./pages/ReactMemoPage";
-import UseCallbackPage from "./pages/UseCallbackPage";
+// import UseCallbackPage from "./pages/UseCallbackPage";
 // import ExamplePage from "./pages/ExamplePage";
 // import ContextPage from "./pages/ContextPage";
 // import SetStatePage from "./pages/SetStatePage";
@@ -14,13 +14,15 @@ import UseCallbackPage from "./pages/UseCallbackPage";
 // import ClassFunctionComponent from "./pages/ClassFunctionComponent";
 // import UseReducerPage from "./pages/UseReducerPage";
 
+import { UseMemoPage, UseCallbackPage } from "./pages/homework9";
+
 export default function App(props) {
   return (
     <div className="app">
       {/* <CommentListPage /> */}
       {/* <PureComponentPage /> */}
       {/* <ReactMemoPage /> */}
-      <UseCallbackPage />
+      {/* <UseCallbackPage /> */}
       {/* <UseMemoPage /> */}
       {/* <ExamplePage /> */}
       {/* <ContextPage /> */}
@@ -30,6 +32,8 @@ export default function App(props) {
       {/* <DiffPage /> */}
       {/* <ClassFunctionComponent /> */}
       {/* <UseReducerPage /> */}
+      <UseCallbackPage></UseCallbackPage>
+      <UseMemoPage></UseMemoPage>
     </div>
   );
 }
